02c5ff3ff4ad777afd3f5b482f7dad67a10257b2
David Silver <dsilver@candidatemetrics.com>
2012-11-08 08:37:16 -0800

    add comments controller and views

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,12 @@
 Blog::Application.routes.draw do
-  resources :photos
-  resources :events
-  resources :articles
+  resources :photos do
+    resources :comments
+  end
+  resources :events  do
+      resources :comments
+    end
+  resources :articles do
+      resources :comments
+    end
   root to: 'articles#index'
 end
